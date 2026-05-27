2b40eab6db26c9fd52df9cc95dbe5cb57e84363f
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-27 17:34:44 -0400

    index.js skeleton

---
+++ index.js
@@ -0,0 +1,11 @@
+import express, { urlencoded } from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
