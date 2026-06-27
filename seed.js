7e23c4a97289544d539b3f3cdb991e0d32f0d3c7
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-27 19:20:32 -0400

    Added seeding for users and posts

--- seed.js
+++ seed.js
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import db from "./db/query.js";
+import { prisma } from "./lib/prisma.js";
 
 const capitalize = (word) => {
   const cap = word.charAt(0).toUpperCase() + word.slice(1);
@@ -21,12 +22,39 @@ const fakerUser = () => {
   return user;
 };
 
+const fakerPost = (user) => {
+  const post = {
+    userId: user[0].id,
+    content: faker.lorem.sentences({ min: 1, max: 3 }),
+    title: faker.lorem.sentence(),
+  };
+  return post;
+};
+
 (async () => {
   const fakerRounds = 10;
-  console.log("Seeding");
+  console.log("Seeding Users");
   for (let i = 0; i < fakerRounds; i++) {
     const user = fakerUser();
     const createdUser = await db.register(user.username, user.password);
     console.log(createdUser);
   }
+  console.log("Done");
+  console.log("Seeding Posts");
+  const userCount = await prisma.user.count();
+
+  for (let i = 0; i < 30; i++) {
+    const skip = Math.floor(Math.random() * userCount);
+    const user = await prisma.user.findMany({
+      take: 1,
+      skip,
+    });
+    const post = fakerPost(user);
+    const createPost = await db.createPost(
+      post.userId,
+      post.title,
+      post.content,
+    );
+  }
+  console.log("Done");
 })();
